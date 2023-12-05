7013201c7737863d5ea5cba2234bce295ff72975
Tharindu Nanayakkara <dasuntn@gmail.com>
2023-12-05 17:15:06 +0100

    percentage visibility control option added.

---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+export const DEFAULT_COLORS = [
+    "#F49F61",
+    "#4A62E8",
+    "#E5AFEE",
+    "#43B77E",
+    "#EA7162",
+    "#D3D57B",
+    "#A2A1F2",
+    "#8CC7D9",
+    "#AE29C7",
+    "#C0E1EC",
+    "#4D8393",
+    "#F4C1B8",
+  ];
